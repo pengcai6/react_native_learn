91aec5566d4d192537540dde5ddf8cfb3c73c153
cai <3538616223@qq.com>
2025-11-23 22:28:51 +0800

    feat: Initialize project structure and add essential resources
    - Created night mode colors.xml and default colors.xml for Android.
    - Added .gitignore for iOS to exclude unnecessary files.
    - Set up bridging header for Swift compatibility in iOS.
    - Created entitlements file for iOS project.
    - Added app icon image for iOS.
    - Defined splash screen background color in colorset for iOS.
    - Implemented SplashScreen storyboard for iOS with layout constraints.
    - Configured Expo.plist for update settings in iOS.
    - Updated Podfile properties for Expo development client.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  presets: [
-    'module:@react-native/babel-preset',
-    'nativewind/babel',
-  ],
-  plugins: [
-    'react-native-reanimated/plugin',
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
+    ],
+  };
 };
