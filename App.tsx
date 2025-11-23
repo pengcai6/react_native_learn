2de5b0b84fd0c788246c5fcba884b87d7f5b7fef
cai <3538616223@qq.com>
2025-11-23 16:03:45 +0800

    chore: update dependencies and add Tailwind CSS configuration
    - Added @react-native/new-app-screen and nativewind to dependencies
    - Updated react-native to version 0.82.1 and added react-native-reanimated and react-native-worklets
    - Added tailwindcss to devDependencies
    - Created tailwind.config.js for Tailwind CSS setup
    - Updated tsconfig.json to include nativewind-env.d.ts

--- App.tsx
+++ App.tsx
@@ -1,33 +1,12 @@
-import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
-
-const Flex = () => {
+import "./global.css"
+import { Text, View } from "react-native";
+ 
+export default function App() {
   return (
-    <View
-      style={[
-        styles.container,
-        {
-          // Try setting `flexDirection` to `"row"`.
-          flexDirection: 'column',
-        },
-      ]}
-    >
-      <View style={{ flex: 1, backgroundColor: 'red' }} />
-      <View style={{ flex: 2, backgroundColor: 'darkorange' }} />
-      <View style={{ flex: 3, backgroundColor: 'green' }} />
-      <Image
-        source={{ uri: 'https://legacy.reactjs.org/logo-og.png' }}
-        style={{ width: 400, height: 400 }}
-      />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="bg-white ">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-});
-
-export default Flex;
+}
